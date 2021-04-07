808d66cc0ad82bbb1671885804b39790ab830ebc
vladimir_cher <vladimircher03@gmail.com>
2021-04-07 18:46:48 +0500

    Add third exercise solution

--- src/test/kotlin/MainClass.kt
+++ src/test/kotlin/MainClass.kt
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test
 
 class MainClass {
     private val classNumber = 20
+    private val classString = "Hello, world"
 
     private fun getLocalNumber(): Int {
 
@@ -18,11 +19,24 @@ class MainClass {
 
     private fun getClassNumber(): Int {
         return classNumber
-
     }
 
     @Test
     fun testGetClassNumber() {
         Assert.assertTrue("Function getClassNumber return number smaller than 45", getClassNumber() > 45)
+    }
+
+    fun getClassString(): String {
+        return classString
+    }
+
+    @Test
+    fun testGetClassString() {
+        val helloRegex = Regex("[Hh]ello")
+
+        Assert.assertTrue(
+            "Function getClassString return string without hello or Hello substring",
+            getClassString().contains(helloRegex)
+        )
     }
 }
